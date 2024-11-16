247983e8c75d884f982d83e6ecf886d0e8c5250c
Jérémy <jeremy@MacBook-Pro-de-Jeremy.local>
2024-11-16 17:34:47 +0100

    fix user granted

--- src/Controller/SensorController.php
+++ src/Controller/SensorController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/sensor')]
+#[IsGranted('ROLE_ADMIN')]
 final class SensorController extends AbstractController
 {
     #[Route(name: 'app_sensor_index', methods: ['GET'])]
--- src/Controller/SensorDataController.php
+++ src/Controller/SensorDataController.php
@@ -11,8 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/sensordata')]
+#[IsGranted('ROLE_ADMIN')]
 final class SensorDataController extends AbstractController
 {
     #[Route(name: 'app_sensor_data_index', methods: ['GET'])]
